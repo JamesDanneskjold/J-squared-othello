f5413afa19626132507113c6892766c3910f083d
JamesDanneskjold <jamesdeacon314@gmail.com>
2016-03-08 11:10:01 -0800

    Almost done, just minimax testing left

--- player.cpp
+++ player.cpp
@@ -12,7 +12,8 @@
  * on (BLACK or WHITE) is passed in as "side". The constructor must finish 
  * within 30 seconds.
  */
-Player::Player(Side side) {
+Player::Player(Side side) 
+{
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
     this->side = side;
@@ -44,16 +45,16 @@ Move* Player::findMove()
                      return move;
                  }
             }
-       }
+        }
     }
     return NULL;
 }
 
 Move* Player::findMoveHeuristic()
 {
-	int val, best = -10000;
-	Move result = Move(0, 0);
-	if (board.hasMoves(side)) 
+    int val, best = -10000;
+    Move result = Move(0, 0);
+    if (board.hasMoves(side)) 
     {
         Move move = Move(0, 0);
         for (int i = 0; i < 8; i++) 
@@ -63,97 +64,97 @@ Move* Player::findMoveHeuristic()
                  move = Move(i, j);
                  if (board.checkMove(&move, side)) 
                  {
-					 val = evaluateMove(move, board);
-					 val = heuristicChange(move, val);
-					 if(val > best)
-					 {
-						 result = Move(i, j);
-						 best = val;
-					 }
+                     val = evaluateMove(move, board);
+                     val = heuristicChange(move, val);
+                     if(val > best)
+                     {
+                         result = Move(i, j);
+                         best = val;
+                     }
                  }
             }
-       }
+        }
     }
     else
     {
-		return NULL;
-	}
-	Move* output = new Move(result);
-	return output;
+	return NULL;
+    }
+    Move* output = new Move(result);
+    return output;
 }
 
 Move* Player::findMoveMinimax()
 {
-	int val, best = -10000;
-	Move result = Move(0, 0);
-	if (board.hasMoves(side))
-	{
-		Move move = Move(0, 0);
-		for (int i = 0; i < 8; i++)
-		{
-			for (int j = 0; j < 8; j++)
-			{
-				move = Move(i, j);
-				if (board.checkMove(&move, side))
-				{
-					val = findWorst(move);
-					if (val > best)
-					{
-						result = Move(i, j);
-						best = val;
-					}
-				}
-			}
-		}
-	}
-	else
-	{
-		return NULL;
-	}
-	Move* output = new Move(result);
-	return output;
+    int val, best = -10000;
+    Move result = Move(0, 0);
+    if (board.hasMoves(side))
+    {
+        Move move = Move(0, 0);
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                move = Move(i, j);
+                if (board.checkMove(&move, side))
+                {
+                    val = findWorst(move);
+                    if (val > best)
+                    {
+                        result = Move(i, j);
+                        best = val;
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        return NULL;
+    }
+    Move* output = new Move(result);
+    return output;
 }
 
 int Player::evaluateMove(Move move, Board testboard)
 {
-	Board tempboard = testboard;
-	Move *testmove = &move;
-	tempboard.doMove(testmove, side);
-	int val = tempboard.countBlack() - tempboard.countWhite();
-	if(side == 0)
-	{
-		val *= -1;
-	}
-	return val;
+    Board tempboard = testboard;
+    Move *testmove = &move;
+    tempboard.doMove(testmove, side);
+    int val = tempboard.countBlack() - tempboard.countWhite();
+    if(side == 0)
+    {
+        val *= -1;
+    }
+    return val;
 }
 
 int Player::heuristicChange(Move move, int val)
 {
-	if (move.getX() == 0 || move.getX() == 7)
-	{
-	     if (move.getY() == 0 || move.getY() == 7)
-		 {
-			 val += 40;
-		 }
-		 else
-		 {
-			 val += 20;
-		 }
-	}
-	else if (move.getY() == 0 || move.getY() == 7)
-	{
+    if (move.getX() == 0 || move.getX() == 7)
+    {
+        if (move.getY() == 0 || move.getY() == 7)
+        {
+            val += 40;
+        }
+        else
+        {
+            val += 20;
+        }
+    }
+    else if (move.getY() == 0 || move.getY() == 7)
+    {
         val += 20;
-	}
-	return val;
+    }
+    return val;
 }
 
 int Player::findWorst(Move move)
 {
-	Board tempboard = board;
-	Move *firstmove = &move;
-	tempboard.doMove(firstmove, side);
-	int val, worst = tempboard.countBlack() - tempboard.countWhite();
-	if (tempboard.hasMoves(OppSide())) 
+    Board tempboard = board;
+    Move *firstmove = &move;
+    tempboard.doMove(firstmove, side);
+    int val, worst = tempboard.countBlack() - tempboard.countWhite();
+    if (tempboard.hasMoves(OppSide())) 
     {
         Move move = Move(0, 0);
         for (int i = 0; i < 8; i++) 
@@ -163,11 +164,11 @@ int Player::findWorst(Move move)
                  move = Move(i, j);
                  if (tempboard.checkMove(&move, OppSide())) 
                  {
-					 val = evaluateMove(move, tempboard);
-					 if(val < worst)
-					 {
-						 worst = val;
-					 }
+                     val = evaluateMove(move, tempboard);
+                     if(val < worst)
+                     {
+                         worst = val;
+                     }
                  }
             }
        }
@@ -189,24 +190,27 @@ int Player::findWorst(Move move)
  * return NULL.
  */
 Move *Player::doMove(Move *opponentsMove, int msLeft) {
-	/*
-    //doAMove(opponentsMove, -1);
+    // This is the very naive initial AI that just pick
+    // the first move it finds.
+    
+    /*
     board.doMove(opponentsMove, OppSide());
     Move* move = findMove();
-    //doAMove(move, 1);
     board.doMove(move, side);
     return move;
     */
-    
-    /*board.doMove(opponentsMove, OppSide());
-    Move* move = findMoveHeuristic();
-	board.doMove(move, side);
-	return move;*/
-	
-	board.doMove(opponentsMove, OppSide());
-	Move* move = findMoveMinimax();
-	board.doMove(move, side);
-	return move;
-	
-	
+    if(testingMinimax)
+    {
+		board.doMove(opponentsMove, OppSide());
+        Move* move = findMoveMinimax();
+        board.doMove(move, side);
+        return move;
+    }
+	else
+    {	
+        board.doMove(opponentsMove, OppSide());
+        Move* move = findMoveHeuristic();
+        board.doMove(move, side);
+        return move;
+    }
 }
